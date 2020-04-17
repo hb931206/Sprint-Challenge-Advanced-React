13415c66d68beadc52cae1dd55b3bdddded58db2
Henry Brophy <henrybrophy@henrys-mbp.lan>
2020-04-17 19:57:13 -0400

    Added my data.

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import axios from "axios";
 
 function App() {
+  const [soccer, setSoccer] = useState([]);
+  useEffect(() => {
+    axios
+      .get("http://localhost:5000/api/players")
+      .then((res) => setSoccer(res.data))
+
+      .catch((err) => console.log(err));
+  }, []);
+  console.log(soccer);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Learn React</p>
       </header>
     </div>
   );
