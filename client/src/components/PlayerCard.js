e4450b76016d3cdbf9e030405a97e2f4a198711e
Henry Brophy <henrybrophy@henrys-mbp.lan>
2020-04-17 21:46:26 -0400

    Tests worked.

--- client/src/components/PlayerCard.js
+++ client/src/components/PlayerCard.js
@@ -2,8 +2,9 @@ import React from "react";
 const PlayerCard = (props) => {
   console.log(props);
   return (
-    <div className="player-card">
+    <div className="player-card" data-testid="card">
       <h1>{props.name}</h1>
+
       <p>{props.country}</p>
     </div>
   );
